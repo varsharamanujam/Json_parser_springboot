950f7440c9ae7f7aa6d2a3cc33398bbfd139db02
varsharamanujam <60292603+varsharamanujam@users.noreply.github.com>
2022-05-27 13:22:14 +0530

    Update controller.java

--- src/main/java/com/example/demo/controller/controller.java
+++ src/main/java/com/example/demo/controller/controller.java
@@ -1,44 +1,30 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.controller.models.Concert;
 import com.example.demo.controller.models.Program;
 import com.example.demo.controller.models.Root;
 import com.example.demo.controller.models.Work;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.core.JsonParseException;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.*;
 
 @Controller
 class controller {
-	
-	static Root root = null;
-    
-	{
-		final ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		File file = new File("D:\\Users\\vsuresh\\Desktop\\largefile-jackson\\final\\demo\\src\\main\\resources\\raw_nyc_phil.json");
+    static Root root = null;
+    {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        File file = new File("D:\\Users\\vsuresh\\Desktop\\largefile-jackson\\demo\\src\\main\\resources\\raw_nyc_phil.json");
         root = objectMapper.readValue(file, Root.class);
-	}
-        
-    
-
-
+    }
     controller() throws IOException {
     }
 
@@ -47,49 +33,73 @@ class controller {
     @ResponseBody
     public int hello() {
         int count = 0;
-        
-                count = (int) root.programs.stream().filter(x->x.programID>10000).count();
-                //root.programs.stream().forEach(y->System.out.println(y.orchestra));
-
-
+        count = (int) root.programs.stream().filter(x->x.programID>10000).count();
         return count;
     }
 
     @RequestMapping("/hello1")
     @ResponseBody
-    public static @NotNull ArrayList<String> hello1() {
+    public static ArrayList<String> hello1() {
         ArrayList<String> prog = new ArrayList<String>();
         root.programs.stream().forEach(y->prog.add(String.valueOf(y.orchestra)));
-        //System.out.println(root.programs.get(0).programID);
-        //root.programs.stream().forEach(y->System.out.println(y.programID));
-        //count = (int) root.programs.stream().filter(x->x.programID>10000).count();
-        //str = root.programs.stream().forEach(y->System.out.println(y.orchestra));
         return prog;
     }
-    
+
     @RequestMapping("/hello2/{id}")
     @ResponseBody
     public String hello2(@PathVariable int id ) {
-        
-   
-    	
-    	Program count = root.programs.get(id);
-    	
-    	
-    	
+        Program count = root.programs.get(id);
         return count.id;
     }
-    
+
     @RequestMapping("/hello3/{id}")
     @ResponseBody
-    public String hello21(@PathVariable int id ) {
-        
-   
-    	
-    	Program count = root.programs.get(id);
-    	
-    	
-    	
-        return count.id;
+    public List<Concert> hello3(@PathVariable String id )  {
+        List<Concert> ret = new ArrayList<Concert>();
+        for (Program p : root.programs){
+            if(id.equals(p.season))
+            {
+                ret.addAll(p.concerts) ;
+            }
+            System.out.println(id+" "+p.season);
+        }
+        return ret;
     }
+
+    @RequestMapping("/hello4/{id}")
+    @ResponseBody
+    public List<Concert> hello4(@PathVariable String id )  {
+        List<Concert> ret = new ArrayList<Concert>();
+        for (Program p : root.programs){
+            for (Work w : p.works){
+                if(id.equals(w.iD))
+                {
+                    ret.addAll(p.concerts) ;
+                    break;
+                }
+                System.out.println(id+" "+p.season);
+            }
+        }
+        return ret;
+    }
+
+    @RequestMapping("/hello5/{id}")
+    @ResponseBody
+    public Set<String> hello5(@PathVariable String id )  {
+        Set<String> s = new HashSet<String>();
+        List<Concert> ret = new ArrayList<Concert>();
+        for (Program p : root.programs){
+            for (Work w : p.works){
+                if(w.composerName!= null && w.composerName.contains(id)) {
+                    for (Concert c : p.concerts) {
+                        s.add(c.venue);
+                    }
+                    break;
+                }
+            }
+
+        }
+        return s;
+    }
+
 }

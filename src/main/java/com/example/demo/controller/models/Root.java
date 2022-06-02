7b1af4918107c404759bd9fd879d98f92b7dc630
varsharamanujam <60292603+varsharamanujam@users.noreply.github.com>
2022-06-02 15:26:25 +0530

    Delete src/main/java/com/example/demo directory

--- src/main/java/com/example/demo/controller/models/Root.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.controller.models;
-
-import java.util.ArrayList;
-
-public class Root{
-    public ArrayList<Program> programs;
-}
--- src/main/java/com/example/demo/controller/models/Soloist.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.controller.models;
-
-public class Soloist{
-    public String soloistName;
-    public String soloistRoles;
-    public String soloistInstrument;
-}
